fc76586e508a5a44a5217eec8e98bfa9c9130ce3
Dragon100000 <layla.schmook@gmail.com>
2025-09-16 19:32:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,8 @@ def webhook():
 @app.route('/')
 def index():
   return "Bot is running!"
+  
+if __name__ == '__main__':
+  import os
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
