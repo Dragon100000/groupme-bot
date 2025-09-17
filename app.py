ec75f936bf86568b7a3d50d42aa310f76466dacc
Dragon100000 <layla.schmook@gmail.com>
2025-09-16 20:10:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def webhook():
   if sender != "clank": # Replace with your actual bot name
     if "?" in text:
       #message=question_response[random(0,14)]
-      message="you responded with a question!!!"
+      message = "you responded with a question!!!"
     else:
       message = f"{sender} said: {text}"
     
